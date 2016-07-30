9119197b9f5b95900e2003f909188cb429129a9b
Gabriel Saraiva <extremez3r0@gmail.com>
2016-07-30 04:03:17 +0300

    Implement saveFile on FilesService

--- app/services/FilesService.scala
+++ app/services/FilesService.scala
@@ -1,12 +1,19 @@
 package services
 
+import scalax.io.Resource
+
 
 class FilesService {
 
-  def load(filePath:String) = scala.io.Source.fromFile(filePath)
+  def load(filePath: String) = scala.io.Source.fromFile(filePath)
 
-  def loadAsString(filePath:String): String = {
+  def loadAsString(filePath: String): String = {
     load(filePath).mkString
+  }
+
+  def save(filePath: String, content: String): Unit = {
+    val f = Resource.fromFile(filePath)
+    f.write(content)
   }
 
 }
